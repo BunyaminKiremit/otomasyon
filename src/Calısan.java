9f4b5bc85d25a9256a930638bea7d22d797ffe0b
Bunyamin4268 <65242555+Bunyamin4268@users.noreply.github.com>
2020-06-13 16:16:22 +0300

    proje

---
+++ src/Calısan.java
@@ -0,0 +1,58 @@
+
+public class Calısan {
+    private int id;
+    private String Ürün_Adi;
+    private String Ürün_Markasi;
+    private String Ürün_Adeti;
+    private String Ürün_Fiyati;
+
+    public Calısan(int id, String Ürün_Adi, String Ürün_Markasi, String Ürün_Adeti, String Ürün_Fiyati) {
+        this.id = id;
+        this.Ürün_Adi = Ürün_Adi;
+        this.Ürün_Markasi = Ürün_Markasi;
+        this.Ürün_Adeti = Ürün_Adeti;
+        this.Ürün_Fiyati = Ürün_Fiyati;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getÜrün_Adi() {
+        return Ürün_Adi;
+    }
+
+    public void setÜrün_Adi(String Ürün_Adi) {
+        this.Ürün_Adi = Ürün_Adi;
+    }
+
+    public String getÜrün_Markasi() {
+        return Ürün_Markasi;
+    }
+
+    public void setÜrün_Markasi(String Ürün_Markasi) {
+        this.Ürün_Markasi = Ürün_Markasi;
+    }
+
+    public String getÜrün_Adeti() {
+        return Ürün_Adeti;
+    }
+
+    public void setÜrün_Adeti(String Ürün_Adeti) {
+        this.Ürün_Adeti = Ürün_Adeti;
+    }
+
+    public String getÜrün_Fiyati() {
+        return Ürün_Fiyati;
+    }
+
+    public void setÜrün_Fiyati(String Ürün_Fiyati) {
+        this.Ürün_Fiyati = Ürün_Fiyati;
+    }
+    
+    
+} 
---
+++ src/Database.java
@@ -0,0 +1,9 @@
+
+public class Database {
+    public static final String user="root";
+    public static final String parola="";
+    public static final String db_ismi="bunyamin";
+    public static final String host="localhost";
+    public static final int port=3306;
+    
+}
